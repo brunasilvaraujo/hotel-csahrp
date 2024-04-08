6ce23edaa75df7fcc9b2ad52a65ca56aacf775cf
brunasilvaraujo <bruna-nascimento_@hotmail.com>
2024-04-08 18:05:35 -0300

    Adicione o atributo State na model City

--- src/TrybeHotel/Models/City.cs
+++ src/TrybeHotel/Models/City.cs
@@ -4,7 +4,13 @@ namespace TrybeHotel.Models
     using System.ComponentModel.DataAnnotations.Schema;
 
     // 1. Adicione o atributo State na model City
-    public class City {
+    public class City
+    {
+        [Key]
+        public int CityId { get; set; }
+        public string? Name { get; set; }
+        public string? State { get; set; }
 
+        public ICollection<Hotel>? Hotels { get; set; }
     }
 }
